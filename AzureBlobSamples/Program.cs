16dade898d5f0ad261fb05c84ab543951928102b
Václav Ryška <ryska.v@gmail.com>
2021-02-05 16:57:03 +0100

    Add project files.

---
+++ AzureBlobSamples/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureBlobSamples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+            .Build();
+        }
+    }
+}
