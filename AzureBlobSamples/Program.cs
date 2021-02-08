009c9fc54d861df3b91a5327251e54602124154f
Václav Ryška <ryska.v@gmail.com>
2021-02-08 22:17:33 +0100

    Azure Blob Storage basic samples.

--- AzureBlobSamples/Program.cs
+++ AzureBlobSamples/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -7,13 +10,76 @@ namespace AzureBlobSamples
 {
     class Program
     {
-        static async Task Main(string[] args)
+        private static BlobServiceClient blobServiceClient;
+        private static BlobContainerClient containerClient;
+        private static BlobClient blobClient;
+
+        private const string fileName = "itixo-logo-default.png";
+
+        static async Task Main()
         {
-            IConfiguration config = new ConfigurationBuilder()
+            CreateDemo();
+            await CreateContainer();
+            await UploadBlob();
+            await ListBlobs();
+            await DownloadBlob();
+            await DeleteContainer();
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            return new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
             .Build();
         }
+
+        private static void CreateDemo()
+        {
+            IConfiguration configuration = GetConfiguration();
+            string connectionString = configuration.GetConnectionString("az204storagesample");
+
+            blobServiceClient = new BlobServiceClient(connectionString);
+            string containerName = "pictures-from-portal";
+            containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        }
+
+        private static async Task CreateContainer()
+        {
+            string containerName2 = "pictures-from-portal-2";
+            containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName2);
+        }
+
+        private static async Task UploadBlob()
+        {
+            string path = "Data";
+            blobClient = containerClient.GetBlobClient(fileName);
+
+            string localFilePath = Path.Combine(path, fileName);
+            var response = await blobClient.UploadAsync(localFilePath, overwrite: true);
+        }
+
+        private static async Task ListBlobs()
+        {
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                Console.WriteLine($"Blobs in container {containerClient.Name}:");
+                Console.WriteLine("\t" + blobItem.Name);
+            }
+        }
+
+        private static async Task DownloadBlob()
+        {
+            blobClient = containerClient.GetBlobClient(fileName);
+            var download = await blobClient.DownloadToAsync(fileName);
+        }
+
+        private static async Task DeleteContainer()
+        {
+            await containerClient.DeleteAsync();
+        }
+
+
     }
 }
